52200d37f3140390eecb9f898cc0ec41a9aa4fa2
Alberto De Pena <depenaj@rushenterprises.com>
2020-02-28 10:27:12 -0600

    Adding unit tests

---
+++ Functions.Infrastructure.Tests/HttpFunctionContextFactoryTests.cs
@@ -0,0 +1,60 @@
+﻿using Xunit;
+using NSubstitute;
+using System.Net.Http;
+using Microsoft.Extensions.Logging;
+
+namespace Functions.Infrastructure.Tests
+{
+    public class HttpFunctionContextFactoryTests
+    {
+        [Fact]
+        public void Bootstrap_With_Valid_Inputs_Should_Have_A_HttpFunctionContext()
+        {
+            var httpFunctionContextFactory = new HttpFunctionContextFactory();
+
+            var httpFunctionContext = httpFunctionContextFactory.Bootstrap(Substitute.For<HttpRequestMessage>(), Substitute.For<ILogger>());
+
+            Assert.NotNull(httpFunctionContext);
+        }
+
+        [Fact]
+        public void Bootstrap_With_Valid_Inputs_Should_Have_A_Request()
+        {
+            var httpFunctionContextFactory = new HttpFunctionContextFactory();
+
+            var httpFunctionContext = httpFunctionContextFactory.Bootstrap(Substitute.For<HttpRequestMessage>(), Substitute.For<ILogger>());
+
+            Assert.NotNull(httpFunctionContext.Request);
+        }
+
+        [Fact]
+        public void Bootstrap_With_Valid_Inputs_Should_Have_A_Logger()
+        {
+            var httpFunctionContextFactory = new HttpFunctionContextFactory();
+
+            var httpFunctionContext = httpFunctionContextFactory.Bootstrap(Substitute.For<HttpRequestMessage>(), Substitute.For<ILogger>());
+
+            Assert.NotNull(httpFunctionContext.Logger);
+        }
+
+        [Fact]
+        public void Bootstrap_With_Valid_Inputs_Should_Have_A_Null_Response()
+        {
+            var httpFunctionContextFactory = new HttpFunctionContextFactory();
+
+            var httpFunctionContext = httpFunctionContextFactory.Bootstrap(Substitute.For<HttpRequestMessage>(), Substitute.For<ILogger>());
+
+            Assert.Null(httpFunctionContext.Response);
+        }
+
+        [Fact]
+        public void Bootstrap_With_Valid_Inputs_Should_Have_A_Null_User()
+        {
+            var httpFunctionContextFactory = new HttpFunctionContextFactory();
+
+            var httpFunctionContext = httpFunctionContextFactory.Bootstrap(Substitute.For<HttpRequestMessage>(), Substitute.For<ILogger>());
+
+            Assert.Null(httpFunctionContext.User);
+        }
+    }
+}
---
+++ Functions.Infrastructure.Tests/HttpFunctionContextTests.cs
@@ -0,0 +1,23 @@
+using System;
+using Xunit;
+using NSubstitute;
+using Microsoft.Extensions.Logging;
+using System.Net.Http;
+
+namespace Functions.Infrastructure.Tests
+{
+    public class HttpFunctionContextTests
+    {
+        [Fact]
+        public void Constructor_With_Null_Request_Should_Throw_Exception()
+        {
+            Assert.Throws<ArgumentNullException>(() => new HttpFunctionContext(null, Substitute.For<ILogger>()));
+        }
+
+        [Fact]
+        public void Constructor_With_Null_Logger_Should_Throw_Exception()
+        {
+            Assert.Throws<ArgumentNullException>(() => new HttpFunctionContext(Substitute.For<HttpRequestMessage>(), null));
+        }
+    }
+}
