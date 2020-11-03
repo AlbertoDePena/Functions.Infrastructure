414617b37fa2256bf38648c72fb67a53f31173e1
Alberto De Pena <depenaj@rushenterprises.com>
2020-11-03 07:26:38 -0600

    Refactoring

--- Functions.CustomBindings/AccessTokenBinding.cs
+++ Functions.CustomBindings/AccessTokenBinding.cs
@@ -15,19 +15,24 @@ namespace Functions.CustomBindings
         private const string AUTH_HEADER_NAME = "Authorization";
         private const string BEARER_PREFIX = "Bearer ";
 
-        private readonly string _b2cMetadataAddress;
-        private readonly string _aadMetadataAddress;
         private readonly string _clientId;
 
-        private TokenValidationParameters _b2cTokenValidationParameters;
-        private TokenValidationParameters _aadTokenValidationParameters;
+        private readonly ConfigurationManager<OpenIdConnectConfiguration> _b2cConfigurationManager;
+        private readonly ConfigurationManager<OpenIdConnectConfiguration> _aadConfigurationManager;
 
         public AccessTokenBinding()
         {
-            // TODO: validate value existance
-            _b2cMetadataAddress = Environment.GetEnvironmentVariable("B2C_METADATA_ADDRESS");
-            _aadMetadataAddress = Environment.GetEnvironmentVariable("AAD_METADATA_ADDRESS");
+            var b2cMetadataAddress = Environment.GetEnvironmentVariable("B2C_METADATA_ADDRESS");
+            var aadMetadataAddress = Environment.GetEnvironmentVariable("AAD_METADATA_ADDRESS");
             _clientId = Environment.GetEnvironmentVariable("CLIENT_ID");
+
+            _b2cConfigurationManager = 
+                new ConfigurationManager<OpenIdConnectConfiguration>(
+                    b2cMetadataAddress, new OpenIdConnectConfigurationRetriever());
+
+            _aadConfigurationManager = 
+                new ConfigurationManager<OpenIdConnectConfiguration>(
+                    aadMetadataAddress, new OpenIdConnectConfigurationRetriever());
         }
 
         public Task<IValueProvider> BindAsync(BindingContext context)
@@ -61,15 +66,21 @@ namespace Functions.CustomBindings
                 return AccessTokenResult.NoToken();
             }
 
-            await SetTokenValidationParametersAsync();
-
             var handler = new JwtSecurityTokenHandler();
 
             Exception b2cException;
 
             try
             {
-                var claimsPrincipal = handler.ValidateToken(bearerToken, _b2cTokenValidationParameters, out _);
+                var connectConfiguration = await _b2cConfigurationManager.GetConfigurationAsync();
+
+                var claimsPrincipal = handler.ValidateToken(bearerToken, new TokenValidationParameters()
+                {
+                    ValidAudience = _clientId,
+                    ValidIssuer = connectConfiguration.Issuer,
+                    IssuerSigningKeys = connectConfiguration.SigningKeys,
+                    ValidateIssuerSigningKey = true
+                }, out _);
 
                 return AccessTokenResult.Success(claimsPrincipal);
             }
@@ -86,7 +97,15 @@ namespace Functions.CustomBindings
 
             try
             {
-                var claimsPrincipal = handler.ValidateToken(bearerToken, _aadTokenValidationParameters, out _);
+                var connectConfiguration = await _aadConfigurationManager.GetConfigurationAsync();
+
+                var claimsPrincipal = handler.ValidateToken(bearerToken, new TokenValidationParameters()
+                {
+                    ValidAudience = _clientId,
+                    ValidIssuer = connectConfiguration.Issuer,
+                    IssuerSigningKeys = connectConfiguration.SigningKeys,
+                    ValidateIssuerSigningKey = true
+                }, out _);
 
                 return AccessTokenResult.Success(claimsPrincipal);
             }
@@ -100,39 +119,6 @@ namespace Functions.CustomBindings
             }
 
             return AccessTokenResult.Error(b2cException, aadException);
-        }
-
-        private async Task SetTokenValidationParametersAsync()
-        {
-            // TODO: bust cache every x minutes
-            if (_b2cTokenValidationParameters == null)
-            {
-                var configurationManager = new ConfigurationManager<OpenIdConnectConfiguration>(_b2cMetadataAddress, new OpenIdConnectConfigurationRetriever());
-                var connectConfiguration = await configurationManager.GetConfigurationAsync();
-
-                _b2cTokenValidationParameters = new TokenValidationParameters()
-                {
-                    ValidAudience = _clientId,
-                    ValidIssuer = connectConfiguration.Issuer,
-                    IssuerSigningKeys = connectConfiguration.SigningKeys,
-                    ValidateIssuerSigningKey = true
-                };
-            }
-
-            // TODO: bust cache every x minutes
-            if (_aadTokenValidationParameters == null)
-            {
-                var configurationManager = new ConfigurationManager<OpenIdConnectConfiguration>(_aadMetadataAddress, new OpenIdConnectConfigurationRetriever());
-                var connectConfiguration = await configurationManager.GetConfigurationAsync();
-
-                _aadTokenValidationParameters = new TokenValidationParameters()
-                {
-                    ValidAudience = _clientId,
-                    ValidIssuer = connectConfiguration.Issuer,
-                    IssuerSigningKeys = connectConfiguration.SigningKeys,
-                    ValidateIssuerSigningKey = true
-                };
-            }
         }
     }
 }
