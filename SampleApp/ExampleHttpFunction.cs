9b298e6f5cc1edca6ffdce387d13a5eadc611fb5
Alberto De Pena <depenaj@rushenterprises.com>
2020-11-03 07:55:40 -0600

    Refactoring

--- SampleApp/ExampleHttpFunction.cs
+++ SampleApp/ExampleHttpFunction.cs
@@ -4,29 +4,54 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.Extensions.Logging;
+using System;
 using System.Threading.Tasks;
+using System.Web.Http;
 
 namespace SampleApp
 {
     public static class ExampleHttpFunction
     {
+        private static async Task<IActionResult> ExecuteAuthenticated(ILogger logger, AccessTokenResult accessTokenResult, Func<Task<IActionResult>> actionResultFunc)
+        {
+            switch (accessTokenResult.Status)
+            {
+                case AccessTokenStatus.Error:
+                    logger.LogError(accessTokenResult.B2CException?.Message);
+                    logger.LogError(accessTokenResult.AADException?.Message);
+
+                    return new InternalServerErrorResult();
+
+                case AccessTokenStatus.Valid:
+                    return await actionResultFunc();
+
+                case AccessTokenStatus.Expired:
+                case AccessTokenStatus.NoToken:
+                    return new UnauthorizedResult();
+
+                default:
+                    logger.LogWarning("Access token result not supported");
+
+                    return new UnauthorizedResult();
+            }
+        }
+
         [FunctionName("ExampleHttpFunction")]
         public static async Task<IActionResult> Run(
             [HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = "example")] HttpRequest request,
             ILogger logger,
             [AccessToken] AccessTokenResult accessTokenResult)
         {
-            if (accessTokenResult.Status == AccessTokenStatus.Error)
+            Task<IActionResult> SayHello()
             {
-                logger.LogError(accessTokenResult.B2CException?.Message);
-                logger.LogError(accessTokenResult.AADException?.Message);
+                var userName = accessTokenResult.Principal?.Identity.Name ?? "anonymous";
+
+                logger.LogInformation($"Request received for {userName}.");
+
+                return Task.FromResult<IActionResult>(new OkObjectResult($"Hello {userName}"));
             }
-            
-            var userName = accessTokenResult.Principal?.Identity.Name ?? "anonymous";
 
-            logger.LogInformation($"Request received for {userName}.");
-
-            return await Task.FromResult(new OkObjectResult(userName));
+            return await ExecuteAuthenticated(logger, accessTokenResult, SayHello);
         }
     }
 }
